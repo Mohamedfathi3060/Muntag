f4f8049af01ca1bbe9a6145ef46ba874fc371fa4
AbdelrahmanMohamed16 <11410120210229@stud.cu.edu.eg>
2024-08-15 02:01:07 +0300

    Finishing SingUp Page Design

--- Template/js/signup.js
+++ Template/js/signup.js
@@ -3,12 +3,14 @@ let nameInput = document.querySelector("#name");
 let identifierInput = document.querySelector("#identifier");
 let passwordInput = document.querySelector("#password");
 let confirmPasswordInput = document.querySelector("#confirmPassword");
+let url = "102.46.211.20:9090";
 
 nameInput.addEventListener("input",function(e){
     checkInput(nameInput,/^[A-Za-z]{3,}[0-9_-]{0,}$/);
     if(nameInput.value == "") {
         nameInput.classList.remove("is-invalid");
         nameInput.classList.remove("is-valid");
+        confirmPasswordInput.classList.add("empty");
     }
 });
 
@@ -19,6 +21,7 @@ identifierInput.addEventListener("input",function(e){
     if(identifierInput.value == "") {
         identifierInput.classList.remove("is-invalid");
         identifierInput.classList.remove("is-valid");
+        confirmPasswordInput.classList.add("empty");
     }
 });
 
@@ -27,11 +30,12 @@ passwordInput.addEventListener("input",function(e){
     if(passwordInput.value == "") {
         passwordInput.classList.remove("is-invalid");
         passwordInput.classList.remove("is-valid");
+        confirmPasswordInput.classList.add("empty");
     }
 });
 
 confirmPasswordInput.addEventListener("input",function(e){
-    if(confirmPasswordInput.value === passwordInput.value){
+    if(confirmPasswordInput.value === passwordInput.value && checkInput(passwordInput,/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)){
         confirmPasswordInput.classList.remove("is-invalid");
         confirmPasswordInput.classList.add("is-valid");
     }
@@ -42,27 +46,52 @@ confirmPasswordInput.addEventListener("input",function(e){
     if(confirmPasswordInput.value == "") {
         confirmPasswordInput.classList.remove("is-invalid");
         confirmPasswordInput.classList.remove("is-valid");
+        confirmPasswordInput.classList.add("empty");
     }
 });
 
 function checkInput(inputField, rule){
     if(inputField.value.match(rule)){
         inputField.classList.remove("is-invalid");
+        inputField.classList.remove("empty");
         inputField.classList.add("is-valid");
         return true;
     }
     else {
         inputField.classList.remove("is-valid");
+        inputField.classList.remove("empty");
         inputField.classList.add("is-invalid");
         return false;
     }
 }
 
-createAccountBtn.addEventListener("click",function (e) {
+createAccountBtn.addEventListener("click",async function (e) {
     if(checkInput(nameInput,/^[A-Za-z0-9_-]{3,15}$/) && (checkInput(identifierInput,/^0(10|11|12|15)[0-9]{8}$/) || checkInput(identifierInput,/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/)) && checkInput(passwordInput,/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/) && passwordInput.value === confirmPasswordInput.value){
-        alert("Good Inputs");
+        await sendData();
     }
     else {
         alert("Bad Inputs");
     }
 });
+
+async function sendData() {
+    try {
+        let data = await fetch(`http://${url}/api/auth/register`,{
+            headers: {
+                "Content-Type": "application/json",
+              },
+            method: "POST",
+            body: JSON.stringify({
+                    "name":nameInput.value,
+                    "email":identifierInput.value,
+                    "password":passwordInput.value,
+                    "password_confirmation":confirmPasswordInput.value
+                }),
+        })
+        let res = data.json();
+        console.log(res);
+    }
+    catch(error){
+        console.log(error.message)
+    }
+}
