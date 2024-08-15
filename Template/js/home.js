88ed45c9ac33ab338428da3ced2b249570970302
AbdelrahmanMohamed16 <11410120210229@stud.cu.edu.eg>
2024-08-15 21:38:59 +0300

    Developing First Section Home Page

--- Template/js/home.js
+++ Template/js/home.js
@@ -6,6 +6,7 @@ let coverProducts = [];
         let layoutProductImg = document.querySelector(".layout .layoutimg img");
         let layoutProductName = document.querySelector(".layout .layouttext p");
         let layoutMarketingLine = document.querySelector(".layout .layouttext h1");
+        let layoutTrackersList = document.querySelector(".layout .layouttrackers ul")
         let coverImagesCount = 1;
         
         async function getCoverProducts(){
@@ -20,17 +21,29 @@ let coverProducts = [];
         }
 
         function layoutDisplay(){
-            console.log("hello");
             let i = Math.ceil(coverImagesCount / 2);
+            for (let j = 0; j < coverImagesCount; j++) {
+                if (j+1 == i) layoutTrackersList.innerHTML += `<li class="bg-secondary rounded-5 mx-1 activetrackerpoint"></li>`;
+                else layoutTrackersList.innerHTML += `<li class="bg-secondary rounded-5 mx-1"></li>`;
+                
+            }
+            let layoutTrackersPoints = document.querySelectorAll(".layout .layouttrackers ul li");
                 setInterval(()=> {
                     console.log(i);
-                    layoutProductImg.src = coverProducts[i].img ? coverProducts[i].img : `imgs/cover-product-${i}.jpeg`;
-                    layoutProductName.innerHTML = coverProducts[i].name ? coverProducts[i].name : `iPhone 14 Series`;
-                    layoutMarketingLine.innerHTML = coverProducts[i].marketLine ? coverProducts[i].marketLine : `Up to 10% off Voucher`;
+                    console.log(layoutTrackersPoints[i-1]);
+                    layoutTrackersPoints[i-1].classList.add("activetrackerpoint");
+                    //coverProducts[i].img ? coverProducts[i].img : 
+                    layoutProductImg.src = `imgs/cover-product-${i}.jpeg`;
+                    //coverProducts[i].name ? coverProducts[i].name :
+                    layoutProductName.innerHTML =  `iPhone 14 Series`;
+                    //coverProducts[i].marketLine ? coverProducts[i].marketLine :
+                    layoutMarketingLine.innerHTML =  `Up to 10% off Voucher`;
                     i++;
                     if (i == coverImagesCount + 1) {
                         i = 1;
                     }
+                    console.log(layoutTrackersPoints[i-1]);
+                    if (coverImagesCount > 1) layoutTrackersPoints[i-1].classList.remove("activetrackerpoint");
                 },5000)
         }
         layoutDisplay();
