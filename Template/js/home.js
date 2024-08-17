10abf4acd9357b74a12dad5d4f81ff4242e498ba
AbdelrahmanMohamed16 <11410120210229@stud.cu.edu.eg>
2024-08-17 22:26:59 +0300

    Finishing Third Section

--- Template/js/home.js
+++ Template/js/home.js
@@ -7,8 +7,24 @@ let coverProducts = [];
         let layoutProductName = document.querySelector(".layout .layouttext p");
         let layoutMarketingLine = document.querySelector(".layout .layouttext h1");
         let layoutTrackersList = document.querySelector(".layout .layouttrackers ul")
+        let cards = document.querySelectorAll(".cards .col-md-2 .card");
         let coverImagesCount = 1;
-        
+        let cardscount = cards.length;
+        let currentCard = Math.ceil(cards.length / 2);
+        let browsebycategorysection = document.querySelector(".browsebycategorysection");
+        let categoriesLeftArrow = document.querySelector(".browsebycategorysection .controlicons .leftarrow");
+        let categoriesRightArrow = document.querySelector(".browsebycategorysection .controlicons .rightarrow");
+        const isInViewport = function(element) {
+            const rect = element.getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+        };
+
+
         async function getCoverProducts(){
             try {
                 let res = await fetch();
@@ -29,8 +45,6 @@ let coverProducts = [];
             }
             let layoutTrackersPoints = document.querySelectorAll(".layout .layouttrackers ul li");
                 setInterval(()=> {
-                    console.log(i);
-                    console.log(layoutTrackersPoints[i-1]);
                     layoutTrackersPoints[i-1].classList.add("activetrackerpoint");
                     //coverProducts[i].img ? coverProducts[i].img : 
                     layoutProductImg.src = `imgs/cover-product-${i}.jpeg`;
@@ -42,12 +56,54 @@ let coverProducts = [];
                     if (i == coverImagesCount + 1) {
                         i = 1;
                     }
-                    console.log(layoutTrackersPoints[i-1]);
                     if (coverImagesCount > 1) layoutTrackersPoints[i-1].classList.remove("activetrackerpoint");
                 },5000)
         }
+
+        function nextCategoryCard(){
+            cards[currentCard].classList.remove("activecard");
+            currentCard++;
+            if (currentCard == cardscount - 1) {
+                currentCard = 0;
+            }
+            cards[currentCard].classList.add("activecard");
+        }
+
+        function prevCategoryCard(){
+            cards[currentCard].classList.remove("activecard");
+            currentCard--;
+            if (currentCard < 0) {
+                currentCard = cardscount - 1;
+            }
+            cards[currentCard].classList.add("activecard");
+        }
+
+        function categoryManager(){
+            cards[currentCard].classList.add("activecard");
+            setInterval(()=> {
+                nextCategoryCard();
+            },5000)
+        }
+
+        categoriesLeftArrow.addEventListener("click", function(){
+            prevCategoryCard();
+        });
+
+        categoriesRightArrow.addEventListener("click", function(){
+            nextCategoryCard();
+        });
+
+        document.addEventListener("keydown", function(e){
+            if (e.code == "ArrowLeft" && isInViewport(browsebycategorysection))
+                 prevCategoryCard();
+        });
+
+        document.addEventListener("keydown", function(e){
+            if (e.code == "ArrowRight" && isInViewport(browsebycategorysection))
+                nextCategoryCard();
+        });
+
         layoutDisplay();
+        categoryManager();
     }
-
-
 )();
